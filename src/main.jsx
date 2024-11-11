62f826c906c2e82dc7c404e9a6ee46808d54af79
Ilham <mi1797128@gmail.com>
2024-11-11 12:13:31 +0700

    feat account address

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // eslint-disable-next-line no-unused-vars
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-
-import './index.css'
-import ErrorPage from './pages/Error.jsx'
-import HomePage from './pages/Homepage.jsx'
-import SignInPage from './pages/Auth/SignInPage.jsx'
-import SignUpPage from './pages/Auth/SignUpPage.jsx'
-import ShopPage from './pages/ShopPage.jsx'
-import Account from './pages/Contact.jsx'
-
+import "./index.css";
+import ErrorPage from "./pages/Error.jsx";
+import HomePage from "./pages/Homepage.jsx";
+import SignInPage from "./pages/Auth/SignInPage.jsx";
+import SignUpPage from "./pages/Auth/SignUpPage.jsx";
+import ShopPage from "./pages/ShopPage.jsx";
+import Account from "./pages/Account.jsx";
 
 // eslint-disable-next-line no-unused-vars
-const router= createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-    errorElement: <ErrorPage/>
-  },{
+    errorElement: <ErrorPage />,
+  },
+  {
     path: "/home",
-    element: <HomePage/>
-  },{
+    element: <HomePage />,
+  },
+  {
     path: "/login",
-    element: <SignInPage/>
-  },{
+    element: <SignInPage />,
+  },
+  {
     path: "/register",
-    element: <SignUpPage/>
-  },{
+    element: <SignUpPage />,
+  },
+  {
     path: "/home/shop",
-    element: <ShopPage/>
-  },{
+    element: <ShopPage />,
+  },
+  {
     path: "/account",
-    element: <Account/>
-  }
-])
+    element: <Account />,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
