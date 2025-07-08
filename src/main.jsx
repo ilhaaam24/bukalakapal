6593a0ef54e26d170897d23d0c420c3ee96c45ef
Ilham <mi1797128@gmail.com>
2025-07-08 16:50:40 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import ErrorPage from "./pages/Error.jsx";
-import HomePage from "./pages/Homepage.jsx";
 import SignInPage from "./pages/Auth/SignInPage.jsx";
 import SignUpPage from "./pages/Auth/SignUpPage.jsx";
 import ShopPage from "./pages/ShopPage.jsx";
@@ -15,11 +14,12 @@ import AccountAddress from "./Components/organisms/AccountAddress.jsx";
 import AccountDetails from "./Components/organisms/AccountDetails.jsx";
 import AccountWishlist from "./Components/organisms/AccountWishlist.jsx";
 import AccountOrders from "./Components/organisms/AccountOrders.jsx";
+import Homepage from "./pages/Homepage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <Homepage />,
     errorElement: <ErrorPage />,
   },
 
