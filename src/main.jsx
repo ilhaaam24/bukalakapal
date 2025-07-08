14bbbcb0427bee2de945172c0b1b2101dd90a6fb
Ilham <mi1797128@gmail.com>
2025-07-08 17:12:10 +0700

    homepage

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import ErrorPage from "./pages/Error.jsx";
+import Homepage from "./pages/Homepage.jsx";
 import SignInPage from "./pages/Auth/SignInPage.jsx";
 import SignUpPage from "./pages/Auth/SignUpPage.jsx";
 import ShopPage from "./pages/ShopPage.jsx";
@@ -14,12 +15,11 @@ import AccountAddress from "./Components/organisms/AccountAddress.jsx";
 import AccountDetails from "./Components/organisms/AccountDetails.jsx";
 import AccountWishlist from "./Components/organisms/AccountWishlist.jsx";
 import AccountOrders from "./Components/organisms/AccountOrders.jsx";
-// import Homepage from "./pages/Homepage.jsx";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <SignInPage />,
+    element: <Homepage/> ,
     errorElement: <ErrorPage />,
   },
 
